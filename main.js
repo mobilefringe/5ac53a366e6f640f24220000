547ccfc6023d4cb49303e31d68b20f5857eee1da
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-05 12:44:57 -0400

    Umber Template 1.0  /main.js

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
                 suggestionAttribute: 'name',
                 newsletter_email: "",
                 stickyMenu: false,
-                isSticky: 750,
+                isSticky: 700,
             }
         },
         created() {
