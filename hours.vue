1fabc02dcca79d83aabcfa6c06943c1abcc66cea
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-06 16:23:39 -0400

    Umber Template 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -11,7 +11,7 @@
             <div class="all_hours_container">
                 <div class="row">
                     <div class="col-sm-12">
-                        <h3 class="hours_heading text-left" >{{$t("hours_page.reg_hours")}}</h3>
+                        <h3 class="hours_heading text-left caps" >{{$t("hours_page.reg_hours")}}</h3>
                         <div id="hours_container" class="hours_container">
                             <div class="hours_div text-left" v-for="hour in hours">
                                 <span>{{hour.day_of_week | moment("dddd", timezone)}} : </span>
