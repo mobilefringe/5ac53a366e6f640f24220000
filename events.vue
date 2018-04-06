5666b95fe64f5bc98656a47371bd3148833e7b59
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-06 15:43:17 -0400

    Umber Template 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -13,7 +13,7 @@
 			<div id="events_container" v-if="events.length > 0">
 				<paginate name="events" v-if="events" :list="events" class="paginate-list margin-60" :per="4">
 					<div class="col-sm-4 event_container" v-for="(promo,index) in paginated('events')" :class="{ 'last': index === (paginated('events').length - 1) }"  :key="promo.id">
-					    <router-link :to="'/promotions/'+ promo.slug" class="event_link">
+					    <router-link :to="'/events/'+ promo.slug" class="event_link">
     						<div class="row event_image_container">
     							<router-link :to="'/promotions/'+ promo.slug" class="event_learn_more">
     								<img v-lazy="promo.store.store_front_url_abs"  class="event_image image" alt=""/>
