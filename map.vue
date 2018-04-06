e2a3172ef8d88f20706e8513cb8f0eb5bbf70c67
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-06 16:10:07 -0400

    Umber Template 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -71,7 +71,6 @@
                         this.pageBanner = {};
                         this.pageBanner.image_url = "//codecloud.cdn.speedyrails.net/sites/5ac53a366e6f640f24220000/image/png/1523030051721/promos_inside_banner.png"
                     }
-                    console.log(temp_repo, this.pageBanner); 
                     this.$on('updateMap', this.updatePNGMap);
                 });
             },
