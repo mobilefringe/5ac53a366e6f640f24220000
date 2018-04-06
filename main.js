a2e54cd4ebaf00f937e42fe46663d728c1aea84b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-06 15:46:24 -0400

    Umber Template 1.0  /main.js

--- main.js
+++ main.js
@@ -149,7 +149,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
             loadData: async function() {
                 try {
                     await this.$store.dispatch('initializeApi', {
-                        site: "twinpines",
+                        site: "twinpin",
                         version: "v4"
                     });
                     await Promise.all([this.$store.dispatch("getData", "property")]);
