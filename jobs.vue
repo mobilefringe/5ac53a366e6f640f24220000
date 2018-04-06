0a6a7a4a41110e0831db18e3e3931cb4645300e0
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-06 16:00:53 -0400

    Umber Template 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -91,7 +91,7 @@
                     if(temp_repo) {
                         // this.pageBanner = temp_repo.images[0];
                         this.pageBanner = {};
-                        this.pageBanner.image_url = "//codecloud.cdn.speedyrails.net/sites/5ac53a366e6f640f24220000/image/png/1523030051721/promos_inside_banner.png"
+                        this.pageBanner.image_url = "//codecloud.cdn.speedyrails.net/sites/5ac53a366e6f640f24220000/image/png/1523030074878/jobs_inside_banner.png"
                     }
                     this.promos = this.promotions;
                 });
