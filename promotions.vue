8bc5224ee9954c39ee8348406444a22a08875014
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-06 14:37:05 -0400

    Umber Template 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -13,12 +13,12 @@
 				<paginate name="promos" v-if="promos" :list="promos" class="paginate-list margin-60" :per="6">
 					<div class="row event_container" v-for="(promo,index) in paginated('promos')" :class="{ 'last': index === (paginated('promos').length - 1) }"  :key="promo.id">
 					    <router-link :to="'/promotions/'+ promo.slug" class="event_learn_more pull-left">
-    						<div class="col-sm-6 col-md-4 event_image_container">
+    						<div class="row event_image_container">
     							<router-link :to="'/promotions/'+ promo.slug" class="event_learn_more">
     								<img v-lazy="promo.store.store_front_url_abs"  class="event_image image" alt=""/>
     							</router-link>
     						</div>
-    						<div class="col-sm-6 col-md-8 event_dets_container">
+    						<div class="row event_dets_container">
     							<h4 class="event_name caps" v-if="locale=='en-ca'">{{promo.name}}</h4>
     							<h4 class="event_name caps" v-else>{{promo.name_2}}</h4>
     							<div v-if="promo.promotionable_type == 'Store'">
