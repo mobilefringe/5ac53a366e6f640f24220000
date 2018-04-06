a7a75c56ab989f7d246d56b8b7f30d559a2b34ce
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-06 15:57:46 -0400

    Umber Template 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -67,9 +67,10 @@
                     this.updatecurrentEvent(this.id);
                     var temp_repo = this.findRepoByName('Events Banner');
                     if(temp_repo) {
-                        this.eventBanner = temp_repo.images[0];
+                        // this.pageBanner = temp_repo.images[0];
+                        this.pageBanner = {};
+                        this.pageBanner.image_url = "//codecloud.cdn.speedyrails.net/sites/5ac53a366e6f640f24220000/image/png/1523030051721/promos_inside_banner.png"
                     }
-                    console.log(this.eventBanner);
                     this.events = this.event;
                 });
             },
