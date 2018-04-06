4f60e0d2e96e4e848f2cc8e4d5f681718cf57cee
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-06 15:25:09 -0400

    Umber Template 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -38,6 +38,8 @@
 					<h3 class="promo_name" style="margin: 20px auto 0px;"  v-if="locale=='en-ca'">{{currentPromo.name}}</h3>
 					<h3 class="promo_name" style="margin: 20px auto 0px;"  v-else>{{currentPromo.name_2}}</h3>
 					<div class="row">
+					    <p class="event_store_name caps" v-if="locale=='en-ca'">{{currentPromo.store.name}}</p>
+	        		    <p class="event_store_name caps" v-else>{{currentPromo.store.name_2}}</p>
 						<p class="promo_div_date pull-left">{{currentPromo.start_date | moment("MMM D", timezone)}} - {{currentPromo.end_date | moment("MMM D", timezone)}}</p>
 					    
 						<social-sharing :url="shareURL(currentPromo.slug)" :title="currentPromo.title" :description="currentPromo.body" :quote="_.truncate(currentPromo.description, {'length': 99})" twitter-user="EastgateSquare" :media="currentPromo.image_url" inline-template >
