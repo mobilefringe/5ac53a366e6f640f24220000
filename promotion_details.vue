af09d496265e29a840bf977cf7e96b58bc37b8d9
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-09 12:31:56 -0400

    Umber Template 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -11,7 +11,7 @@
 		<div class="site_container">
 			<div class="row">
 				<div class="col-sm-12 promo_image_container text-left">
-					<router-link to="/promotions"><i class="fa fa-angle-left"></i> &nbsp;{{$t("promos_page.back_to_promos")}}</router-link>
+					<router-link to="/promotions" class="back_to_btn"><i class="fa fa-angle-left"></i> &nbsp;{{$t("promos_page.back_to_promos")}}</router-link>
 					<h3 class="promo_name" style="margin: 20px auto 0px;"  v-if="locale=='en-ca'">{{currentPromo.name}}</h3>
 					<h3 class="promo_name" style="margin: 20px auto 0px;"  v-else>{{currentPromo.name_2}}</h3>
 					<div class="row">
